056a617994b4f1d27a54275810aefa8422da2d4c
Nuttaphat <nuttaphat.aruno@gmail.com>
2020-09-12 17:24:26 +0800

    create gameplay and result checking methods

--- TheHumbleNishiyamaRandomnessGame/src/game_engine.py
+++ TheHumbleNishiyamaRandomnessGame/src/game_engine.py
@@ -43,3 +43,51 @@ class GameEngine:
         winning_sequence = reference_sequence[:2]
         winning_sequence.insert(0, converted_value)
         return winning_sequence
+    
+    def _check_result(self, computer_sequence: List[str],
+                                player_sequence: List[str],
+                                current_game_sequence: List[str]) -> Dict[str, bool]:
+        """ Check the result of a game
+            All sequences must contain the same length of elements
+            return the result of a player winnig a game or not
+        """
+        result = {
+            'computer_has_won': False,
+            'player_has_won': False
+        }
+
+        if computer_sequence == current_game_sequence:
+            result['computer_has_won'] = True
+
+        if player_sequence == current_game_sequence:
+            result['player_has_won'] = True
+
+        return result
+    
+    def _play(self, is_winning_method_used = False) -> None:
+        """ Play the game """
+        deck = self.all_cards.copy() # Get a dek of cards
+        random.shuffle(deck) # Shuffle a deck (in place)
+
+        # Select patterns
+        computer_sequence = self._card_selection()
+        player_sequence =  self._card_selection_winning(computer_sequence) \
+                                if is_winning_method_used else self._card_selection()
+        
+        # Start playing game
+        current_game_sequence = []
+        final_result = {}
+        while deck:
+            drawn_card = deck.pop()
+            current_game_sequence.append(drawn_card)
+            # Adjust current_game_sequence to contain only three elements
+            if len(current_game_sequence) > 3:
+                current_game_sequence =  current_game_sequence[-3:]
+            # Check the result
+            if len(current_game_sequence) == 3:
+                game_result = self._check_result(computer_sequence, player_sequence, current_game_sequence)
+                if True in game_result.values():
+                    final_result = game_result.copy()
+                    break
+
+        return final_result
