462d663d8bebbc07441e05335f77d45158f91956
Nuttaphat <nuttaphat.aruno@gmail.com>
2020-09-13 19:56:28 +0800

    refactor visualisation procedures

--- TheHumbleNishiyamaRandomnessGame/src/main.py
+++ TheHumbleNishiyamaRandomnessGame/src/main.py
@@ -15,25 +15,27 @@ game_engine = GameEngine()
 
 
 # Main function
-def main():
+def main() -> None:
     # Without a winning method
-    print('Without a winning method')
     without_winning_method_res = game_engine._start_experiment(is_winning_method_used=False)
     
     # With a winning method
-    print('With a winning method')
     with_winning_method_res = game_engine._start_experiment(is_winning_method_used=True)
 
-    # Visualisation
+    # Prepare DataFrame
     df_without_winning_method = pd.DataFrame([without_winning_method_res])
     df_without_winning_method['cat'] = 'without'
     df_with_winning_method_res = pd.DataFrame([with_winning_method_res])
     df_with_winning_method_res['cat'] = 'with'
     df_final = pd.concat([df_without_winning_method, df_with_winning_method_res])
+    df_final = df_final.set_index('cat')
     print(df_final)
 
+    # Visualisation
     df_final.plot.bar()
     plt.show()
+
+    return None
 
 
 # Execute the script
