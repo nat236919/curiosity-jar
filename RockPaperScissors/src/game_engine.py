ba7be1cf29a45906310ac359d993e85e07b94e87
nat236919 <nuttaphat.aruno@gmail.com>
2022-01-24 17:58:28 +0800

    feat: add Rock Paper Scissors game template (#1)

---
+++ RockPaperScissors/src/game_engine.py
@@ -0,0 +1,19 @@
+"""
+FILE: game_engine.py
+DESCRIPTION: A game engine for Rock Paper Scissors against the computer.
+AUTHOR: Nuttaphat Arunoprayoch (@nuttaphat)
+DATE: 24-Jan-2022
+"""
+from typing import List, Dict, Any
+
+
+class RockPaperScissors:
+    """A game engine class
+    """
+
+    def __init__(self):
+        self.choices = ['r', 'p', 's']
+
+    def play(n: int = 1) -> None:
+
+        return None
---
+++ RockPaperScissors/src/main.py
@@ -0,0 +1,15 @@
+"""
+PROJECT: Rock Paper Scissors
+DESCRIPTION: A simple game of rock paper scissors against the computer.
+AUTHOR: Nuttaphat Arunoprayoch (@nuttaphat)
+DATE: 24-Jan-2022
+"""
+
+
+def main():
+    print("Welcome to Rock Paper Scissors!")
+    return None
+
+
+if __name__ == '__main__':
+    main()
