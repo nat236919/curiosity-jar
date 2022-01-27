587ecbd4750d5267604b6c4b25b7772c982114f0
nat236919 <nuttaphat.aruno@gmail.com>
2022-01-27 16:29:41 +0800

    feat(rps): create a private method to detemine results (#1)

--- RockPaperScissors/src/game_engine.py
+++ RockPaperScissors/src/game_engine.py
@@ -16,6 +16,7 @@ class RockPaperScissors:
         self.choices = ['r', 'p', 's']
         self.player_score = 0
         self.computer_score = 0
+        self.draws = 0
         self.times_played = 0
 
     def show_summary(self) -> None:
@@ -26,8 +27,9 @@ class RockPaperScissors:
         else:
             won_rate = self.player_score / self.times_played
             text = f"""
-            --- Summary ----------------------------------------
-                You won {self.player_score} times
+            --------------------- Summary ----------------------
+                Played {self.times_played} times.
+                You won {self.player_score} times.
                 The computer won {self.computer_score} times.
                 Player won {won_rate * 100}% of the time.
             ----------------------------------------------------
@@ -50,22 +52,25 @@ class RockPaperScissors:
             # Get choices
             computer_hand = self.__pick_hand().lower()
             player_hand = str(input('Enter your choice [r, p, s]: ')).lower()
+
+            print(f'Player picked [{player_hand}]')
+            print(f'Computer picked [{computer_hand}]')
+
             if player_hand not in self.choices:
                 print('Invalid choice! Try again.')
                 continue
 
             # Determine winner
-            if computer_hand == player_hand:
-                print(f'Computer picked {computer_hand}')
+            result = self.__determine_result(player_hand, computer_hand)
+
+            if result == 0:
                 print('It\'s a draw!')
+                self.draws += 1
 
-            player_won = self.__player_won(player_hand, computer_hand)
-
-            # Display result and Update stats
-            if player_won:
+            elif result == 1:
                 self.player_score += 1
                 print(f'You won! Computer picked {computer_hand}')
-            else:
+            elif result == 2:
                 self.computer_score += 1
                 print(f'Computer won! Computer picked {computer_hand}')
 
@@ -76,6 +81,25 @@ class RockPaperScissors:
         self.show_summary()
 
         return None
+
+    def __determine_result(self, player_hand: str, computer_hand: str) -> int:
+        """Return the result of a round
+
+        Args:
+            player_hand (str): A hand from the list of choices by player
+            computer_hand (str): A hand from the list of choices by computer
+
+        Returns:
+            int: {
+                0: draw,
+                1: player wins,
+                2: computer wins
+            }
+        """
+        if player_hand == computer_hand:
+            return 0
+
+        return 1 if self.__player_won(player_hand, computer_hand) else 2
 
     def __player_won(self, player_hand: str, computer_hand: str) -> bool:
         """Check if player wins a round
